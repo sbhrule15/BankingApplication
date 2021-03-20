e724dc523d0728554b66fcd17bc388b3967b0ac2
Joshua Pare <joshuabpare@gmail.com>
2021-03-20 15:23:21 -0400

    Added constructor and methods to account class.

--- Account.cpp
+++ Account.cpp
@@ -3,3 +3,44 @@
 //
 
 #include "Account.h"
+
+#include <utility>
+
+// Constructor
+Account::Account(int acctNumber, std::string name, float balance, AccountType accountType) : acctNumber(
+        acctNumber), name(std::move(name)), balance(balance), accountType(accountType) {}
+
+// Functions
+int Account::getAcctNumber() const {
+    return acctNumber;
+}
+
+const std::string &Account::getName() const {
+    return name;
+}
+
+void Account::setName(const std::string &acctName) {
+    Account::name = acctName;
+}
+
+float Account::getBalance() const {
+    return balance;
+}
+
+AccountType Account::getAccountType() const {
+    return accountType;
+}
+
+void Account::setAccountType(AccountType type) {
+    Account::accountType = type;
+}
+
+std::string Account::getAccountTypeString() const {
+    if (accountType == Checking) {
+        return "Checking";
+    } else if (accountType == Savings) {
+        return "Savings";
+    } else {
+        return "N/A";
+    }
+}
--- Account.h
+++ Account.h
@@ -5,10 +5,42 @@
 #ifndef BANKINGAPPLICATION_ACCOUNT_H
 #define BANKINGAPPLICATION_ACCOUNT_H
 
+#include <string>
+
+enum AccountType {
+    Checking, Savings
+};
 
 class Account {
 protected:
+    int acctNumber;
+    std::string name;
+    float balance;
+    AccountType accountType;
 
+public:
+    // constructor
+    Account(int acctNumber, std::string name, float balance, AccountType accountType);
+
+    // pure virtual functions
+    virtual bool deposit(float amt) = 0;
+
+    virtual bool withdraw(float amt) = 0;
+
+    // functions
+    int getAcctNumber() const;
+
+    const std::string &getName() const;
+
+    void setName(const std::string &name);
+
+    float getBalance() const;
+
+    AccountType getAccountType() const;
+
+    std::string getAccountTypeString() const;
+
+    void setAccountType(AccountType type);
 
 };
 
