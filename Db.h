7958ed111bc7e575f0d9ca5023de77b04eed1016
Joshua Pare <joshuabpare@gmail.com>
2021-03-22 09:20:16 -0400

    Created query function declarations in db.h.

--- Db.h
+++ Db.h
@@ -18,12 +18,20 @@ namespace db {
     static int createTables(const char* dbdir);
 
     // Account Queries
-    static int addAccount(const char* dbdir, AccountType accountType, std::string name);
+    static Account addAccount(const char* dbdir, AccountType accountType, std::string name);
     static int deleteAccount(const char* dbdir, int accId);
-    static int deposit(const char* dbdir, int accId);
-    static int withdraw(const char* dbdir, int accId);
+    static float deposit(const char* dbdir, int accId);
+    static float withdraw(const char* dbdir, int accId);
+    static Account getAccountById(const char* dbdir, int accId);
+    static std::vector<Account> getAllAccounts(const char* dbdir );
+
 
     // Transaction Queries
+    static int createTransaction(const char* dbdir, float amtChange, TransactionType transactionType);
+    static std::vector<Transaction> getAllTransactions(const char* dbdir);
+    static std::vector<Transaction> getTransactionsByAccount(const char* dbdir, const int &accountId);
+    static std::vector<Transaction> getTransactionsByType(const char* dbdir, TransactionType transactionType);
+    static Transaction getTransactionById(const char* dbdir, const int transactionId);
 
     // Report Queries
 
