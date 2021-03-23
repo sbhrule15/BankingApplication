0ad21252c9253b89e827e523e4dcf8b4a85ec8de
Joshua Pare <joshuabpare@gmail.com>
2021-03-23 09:23:54 -0400

    Declaration return type changes in db namespace

--- Db.cpp
+++ Db.cpp
@@ -30,7 +30,7 @@ static int exec(const char* dbdir, sqlite3 *DB, std::string query){
 static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
     int i;
     for(i = 0; i<argc; i++) {
-        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+        printf("%s: %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
     }
     printf("\n");
     return 0;
