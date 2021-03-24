175c882a3fb3c98bd3762fc04ecab4702d62d140
Joshua Pare <joshuabpare@gmail.com>
2021-03-24 15:51:30 -0400

    Testing commit after removal of sqlite files in main directory.

---
+++ cmake-build-debug/thirdparty/SQLiteCpp/CMakeFiles/SQLiteCpp.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/joshuapare/CLionProjects/BankingApplication/thirdparty/SQLiteCpp/src/Backup.cpp" "/Users/joshuapare/CLionProjects/BankingApplication/cmake-build-debug/thirdparty/SQLiteCpp/CMakeFiles/SQLiteCpp.dir/src/Backup.cpp.o"
+  "/Users/joshuapare/CLionProjects/BankingApplication/thirdparty/SQLiteCpp/src/Column.cpp" "/Users/joshuapare/CLionProjects/BankingApplication/cmake-build-debug/thirdparty/SQLiteCpp/CMakeFiles/SQLiteCpp.dir/src/Column.cpp.o"
+  "/Users/joshuapare/CLionProjects/BankingApplication/thirdparty/SQLiteCpp/src/Database.cpp" "/Users/joshuapare/CLionProjects/BankingApplication/cmake-build-debug/thirdparty/SQLiteCpp/CMakeFiles/SQLiteCpp.dir/src/Database.cpp.o"
+  "/Users/joshuapare/CLionProjects/BankingApplication/thirdparty/SQLiteCpp/src/Exception.cpp" "/Users/joshuapare/CLionProjects/BankingApplication/cmake-build-debug/thirdparty/SQLiteCpp/CMakeFiles/SQLiteCpp.dir/src/Exception.cpp.o"
+  "/Users/joshuapare/CLionProjects/BankingApplication/thirdparty/SQLiteCpp/src/Savepoint.cpp" "/Users/joshuapare/CLionProjects/BankingApplication/cmake-build-debug/thirdparty/SQLiteCpp/CMakeFiles/SQLiteCpp.dir/src/Savepoint.cpp.o"
+  "/Users/joshuapare/CLionProjects/BankingApplication/thirdparty/SQLiteCpp/src/Statement.cpp" "/Users/joshuapare/CLionProjects/BankingApplication/cmake-build-debug/thirdparty/SQLiteCpp/CMakeFiles/SQLiteCpp.dir/src/Statement.cpp.o"
+  "/Users/joshuapare/CLionProjects/BankingApplication/thirdparty/SQLiteCpp/src/Transaction.cpp" "/Users/joshuapare/CLionProjects/BankingApplication/cmake-build-debug/thirdparty/SQLiteCpp/CMakeFiles/SQLiteCpp.dir/src/Transaction.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SQLITE_ENABLE_COLUMN_METADATA"
+  "SQLITE_ENABLE_JSON1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../thirdparty/SQLiteCpp/sqlite3"
+  "../thirdparty/SQLiteCpp/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/joshuapare/CLionProjects/BankingApplication/cmake-build-debug/thirdparty/SQLiteCpp/sqlite3/CMakeFiles/sqlite3.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
